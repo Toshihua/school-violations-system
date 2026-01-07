556ea0ccb3efd2c07cdf51bd5c4210f7faed8e56
Roman <lopezromanaragorn@gmail.com>
2026-01-07 12:36:48 +0800

    fix: added user reference to dashboardcontroller

--- app/Http/Controllers/Student/DashboardController.php
+++ app/Http/Controllers/Student/DashboardController.php
@@ -8,6 +8,7 @@ use App\Models\Appeal;
 use App\Models\Status;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
